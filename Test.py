a261c8c84399e1a62d0338559e13b9e6e6121445
isa <isak.wadso@gmail.com>
2026-08-27 09:46:17 +0200

    Raspberry Pi GPIO test

---
+++ Test.py
@@ -0,0 +1,17 @@
+cat > blink.py << 'EOF'
+from gpiozero import LED, Button
+from time import sleep
+import gpiozero as gpio
+import time
+
+
+led = LED(18)
+button = Button(25)
+
+while True:
+    if button.is_pressed:
+        led.off()
+    else:
+        led.on()
+    sleep(0.05)
+EOF
