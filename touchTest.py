1b32a2e6c40971a1c1a758d7e59b121a7e754b0d
isa <isak.wadso@gmail.com>
2026-09-17 11:59:13 +0200

    updated touch test

--- touchTest.py
+++ touchTest.py
@@ -5,4 +5,6 @@ ts = Adafruit_STMPE610_SPI(board.SPI(), digitalio.DigitalInOut(board.CE1))
 while True:
     if ts.touched:
         while not ts.buffer_empty:
-            print(ts.touch_point)   # raw (x, y, pressure)
+            x, y, z = ts.touch_point
+            if z > 20:
+                print(to_pixels(x, y, 480, 320))
